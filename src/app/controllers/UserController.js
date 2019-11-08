da1b8b261606f600d614b7689dd0ee630400df49
Jose Augusto Aragao Da Silva <jasilva17@stefanini.com>
2019-11-08 16:10:07 -0300

    feat: commit inicial

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,19 @@
+import Queue from '../lib/Queue';
+
+
+export default {
+    async store(req,res){
+        const { name, email, password } = req.body;
+
+        const user ={
+            name,
+            email,
+            password
+        }
+
+        //adicionar job RegistrationMail na fila
+        await Queue.add('RegistrationMail',{ user })
+
+        return res.json(user)
+    }
+}
